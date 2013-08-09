fcf6629e6dee9795692e601026f3e20555a53e37
Nathan Stokes <nes@lockerz.com>
2013-08-09 18:06:10 -0400

    Added first test

---
+++ tests/AuthTokenTest.php
@@ -0,0 +1,35 @@
+<?php
+
+//use Mockery as m;
+
+class AuthTest extends PHPUnit_Framework_TestCase {
+
+/*
+    public function setUp()
+    {
+
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }
+*/
+
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        /*
+        $crawler = $this->client->request('GET', '/');
+        $this->assertTrue($this->client->getResponse()->isOk());
+        */
+       $test = true;
+
+       $this->assertTrue($test);
+    }
+
+}
