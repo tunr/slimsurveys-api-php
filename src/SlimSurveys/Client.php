2ba42c9ddeeccf349e49280293ba9e88b531763c
Nathan Stokes <nes@lockerz.com>
2013-08-19 16:51:33 -0400

    Major refactor

--- src/SlimSurveys/Client.php
+++ src/SlimSurveys/Client.php
@@ -26,47 +26,53 @@ class Client
     private $token = '';
 
     /**
-     * Response data format
+     * Url path
      *
      * @var string
      */
-    private $format = 'json';
+    private $url = '';
 
     /**
-     * Request headers
+     * Headers
      *
      * @var array
      */
     private $headers = array();
 
     /**
-     * Dynamic curl options
+     * Options
      *
      * @var array
      */
     private $options = array();
 
     /**
-     * Request curl options
+     * Query parameters
      *
      * @var array
      */
-    private $requestOptions = array();
+    private $queries = array();
 
     /**
-     * Default curl options
+     * Post fields
      *
      * @var array
      */
-    private $defaultOptions = array(
-        CURLOPT_CONNECTTIMEOUT => 5,
-        CURLOPT_TIMEOUT        => 10,
-        CURLOPT_USERAGENT      => 'SlimSurveys-API-Client',
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_FOLLOWLOCATION => 1,
-        CURLOPT_FAILONERROR    => 0,
-        CURLOPT_ENCODING       => 'utf-8',
-    );
+    private $fields = array();
+
+    /**
+     * Response data
+     *
+     * @var array
+     */
+    private $data = null;
+
+    /**
+     * Curl info
+     *
+     * @var array
+     */
+    private $info = array();
 
     /**
      * Constructor
@@ -75,24 +81,24 @@ class Client
      *
      * @return void
      */
-    public function __construct($config)
+    public function __construct(array $config)
     {
-        // todo: validate params
-        // todo: make an array of accepted formats for validation
-
-        $this->key    = (!empty($config['key'])) ? $config['key'] : '';
-        $this->format = (!empty($config['format'])) ? $config['format'] : $this->format;
+        $this->key = (!empty($config['key'])) ? (string) $config['key'] : '';
 
         if (!empty($config['token']))
         {
-            $this->headers['X-AUTH-TOKEN'] = $config['token'];
+            $this->token = (string) $config['token'];
         }
     }
+
+// ===================================================================================
+// auth
+// ===================================================================================
 
     /**
      * Get authorization token
      * 
-     * @return json
+     * @return $this
      */
     public function getAuthToken($email, $password)
     {
@@ -101,89 +107,212 @@ class Client
             'password' => $password,
         );
 
-        return $this->post('auth/token', $data);
+        return $this->setUrl('auth/token')
+            ->setFields($data)
+            ->post();
     }
 
-    /**
-     * Get authenticated users account
-     * 
-     * @return json
-     */
+// ===================================================================================
+// answers
+// ===================================================================================
+
+    // get survey answers
+    public function getSurveyAnswers($surveyId, $uvid = null)
+    {
+        $this->setUrl('answers/survey')->applyResourceId('survey_uid', $surveyId);
+
+        return $this->setField('uvid', $uvid)->get();
+    }
+
+    // get question answers
+    public function getQuestionAnswers($questionId)
+    {
+        return $this->setUrl('answers/question/' . $questionId)->get();
+    }
+
+    // create question answer
+    public function createQuestionAnswer($questionId, $answer, $milestone = '', $uvid = '')
+    {
+        $this->setUrl('answers/question/' . $questionId);
+        $this->setField('answer', $answer);
+        $this->setField('milestone', $milestone);
+        $this->setField('uvid', $uvid);
+
+        return $this->post();
+    }
+
+// ===================================================================================
+// identities
+// ===================================================================================
+
+    // create identity
+    public function createIdentity($surveyId, $uvid, $data)
+    {
+        $this->setUrl('answers/question')->applyResourceId('survey_uid', $surveyId);
+        $this->setField('uvid', $uvid);
+        $this->setFields($data);
+
+        return $this->post();
+    }
+
+// ===================================================================================
+// images
+// ===================================================================================
+
+    // get question answers
+    public function getImage($imageId)
+    {
+        return $this->setUrl('images/image/' . $imageId)->get();
+    }
+
+    // create question image
+    public function createQuestionImage($questionId, $file, $position = 0)
+    {
+        $this->setUrl('images/create/' . $questionId);
+        $this->setField('file', $file);
+        $this->setField('position', $positon);
+
+        //$this->setOption(CURLOPT_POSTFIELDS, http_build_query($this->fields, '', '&'));
+        //$this->setOption(CURLOPT_POST, 1);
+        //return $this->request();
+        
+        return $this->post();
+    }
+
+    // delete image
+    public function deleteImage($imageId)
+    {
+        return $this->setUrl('images/image/' . $imageId)->delete();
+    }
+
+// ===================================================================================
+// surveys
+// ===================================================================================
+
+    // get survey
+    public function getSurvey($surveyId)
+    {
+        $this->setUrl('surveys/survey')->applyResourceId('survey_uid', $surveyId);
+
+        return $this->get();
+    }
+
+    // get survey embed
+    public function getSurveyEmbed($surveyId)
+    {
+        $this->setUrl('surveys/embed')->applyResourceId('survey_uid', $surveyId);
+
+        return $this->get();
+    }
+
+    // get user surveys
+    public function getUserSurveys($userId)
+    {
+        $this->setUrl('surveys/user')->applyResourceId('username', $userId);
+
+        return $this->get();
+    }
+
+    // get my surveys
+    public function getMySurveys()
+    {
+        return $this->setUrl('surveys/mine')->get();
+    }
+
+// ===================================================================================
+// questions
+// ===================================================================================
+
+    // get question
+    public function getQuestion($questionId)
+    {
+        return $this->setUrl('questions/question/' . $questionId)->get();
+    }
+
+    // get question answers
+    public function createSurveyQuestion($surveyId)
+    {
+        return $this->setUrl('questions/create/' . $surveyId)->post();
+    }
+
+    // get question answers
+    public function updateQuestion($questionId)
+    {
+        return $this->setUrl('questions/question/' . $questionId)->post();
+    }
+
+    // delete question
+    public function deleteQuestion($questionId)
+    {
+        return $this->setUrl('questions/question/' . $questionId)->delete();
+    }
+
+// ===================================================================================
+// results
+// ===================================================================================
+
+    // get survey results
+    public function getSurveyResults($surveyId)
+    {
+        $this->setUrl('results/survey')->applyResourceId('survey_uid', $surveyId);
+
+        return $this->get();
+    }
+
+    // get question results
+    public function getQuestionResults($questionId)
+    {
+        return $this->setUrl('results/question/' . $questionId)->get();
+    }
+
+// ===================================================================================
+// users
+// ===================================================================================
+
+    // Get authenticated users account
     public function me()
     {
-        return $this->get('users/me');
+        return $this->setUrl('users/me')->get();
     }
 
-    // ===================================================================================
+// ===================================================================================
+// request methods
+// ===================================================================================
 
     /**
-     * Build header
-     *
-     * @return array()
+     * Set url
+     * 
+     * @return $this
      */
-    private function buildHeader()
+    public function setUrl($url)
     {
-        $headers = array();
+        $this->reset();
 
-        foreach ($this->headers as $header => $content)
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Determine request resource id placement
+     * 
+     * @return $this
+     */
+    public function applyResourceId($query, $resourceId)
+    {
+        if (is_string($resourceId))
         {
-            $headers[] = ($content) ? $header . ':' . $content : $header;
+            $this->setQuery($query, $resourceId);
+        }
+        else
+        {
+            $url  = trim($this->url, '/');
+            $url .= '/' . $resourceId;
+
+            $this->url = $url;
         }
 
-        return $headers;
-    }
-
-    /**
-     * Set header entry
-     *
-     * @param $header header entry title
-     * @param $content header entry content
-     * @return SlimSurveys
-     */
-    public function setHeader($header, $content = null)
-    {
-        $this->headers[$header] = $content;
-
         return $this;
-    }
-
-    /**
-     * Set curl config option
-     *
-     * @param $code curl option code
-     * @param $value curl option value
-     * @return SlimSurveys
-     */
-    public function setOption($code, $value)
-    {
-        $this->options[$code] = $value;
-        
-        return $this;
-    }
-
-    /**
-     * Set curl config options
-     *
-     * @param $options curl option code
-     * @return SlimSurveys
-     */
-    public function setOptions($options = array())
-    {
-        foreach ($options as $code => $value) {
-            $this->setOption($code, $value);
-        }
-        
-        return $this;
-    }
-
-    /**
-     * Get curl options
-     *
-     * @return void
-     */
-    public function getOptions()
-    {        
-        return $this->options;
     }
 
     /**
@@ -191,16 +320,13 @@ class Client
      * 
      * @return string
      */
-    private function buildUrl($url, $params)
+    private function buildUrl()
     {
-        $url = self::BASE_URL . trim($url, '/');
+        $url     = self::BASE_URL . trim($this->url, '/');
+        $queries = $this->queries;
 
-        if ('json' !== $this->format) {
-            $params['format'] = $this->format;
-        }
-
-        if (is_array($params) && !empty($params)) {
-            $url .= '?' . http_build_query($params, '', '&');
+        if (!empty($queries)) {
+            $url .= '?' . http_build_query($queries, '', '&');
         }
 
         $this->setOption(CURLOPT_URL, $url);
@@ -209,17 +335,124 @@ class Client
     }
 
     /**
+     * Build header
+     *
+     * @return array()
+     */
+    private function buildHeader()
+    {
+        $this->headers['X-API-KEY'] = $this->key;
+
+        if ($this->token)
+        {
+            $this->headers['X-AUTH-TOKEN'] = $this->token;
+        }
+
+        foreach ($this->headers as $header => $content)
+        {
+            $headers[] = ($content) ? $header . ':' . $content : $header;
+        }
+
+        $this->setOption(CURLOPT_HTTPHEADER, $headers);
+
+        return $this;
+    }
+
+    /**
+     * Set header entry
+     *
+     * @param $key key
+     * @param $value value
+     * @return SlimSurveys
+     */
+    private function setHeader($key, $value = null)
+    {
+        $this->headers[$key] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set config option
+     *
+     * @param $key key
+     * @param $value value
+     * @return SlimSurveys
+     */
+    private function setOption($key, $value)
+    {
+        $this->options[$key] = $value;
+        
+        return $this;
+    }
+
+    /**
+     * Set query parameter
+     *
+     * @param $key key
+     * @param $value value
+     * @return SlimSurveys
+     */
+    private function setQuery($key, $value)
+    {
+        $this->queries[$key] = $value;
+        
+        return $this;
+    }
+
+    /**
+     * Set query parameters
+     *
+     * @param $queries query parameters
+     * @return SlimSurveys
+     */
+    private function setQueries($queries = array())
+    {
+        foreach ($queries as $key => $value)
+        {
+            $this->setQuery($key, $value);
+        }
+        
+        return $this;
+    }
+
+    /**
+     * Set post field
+     *
+     * @param $key key
+     * @param $value value
+     * @return SlimSurveys
+     */
+    private function setField($key, $value)
+    {
+        $this->fields[$key] = $value;
+        
+        return $this;
+    }
+
+    /**
+     * Set post fields
+     *
+     * @param $fields post fields
+     * @return SlimSurveys
+     */
+    private function setFields($fields = array())
+    {
+        foreach ($fields as $key => $value) {
+            $this->setField($key, $value);
+        }
+
+        return $this;
+    }
+
+    /**
      * Get request
      * 
      * @return void
      */
-    private function get($url, $data = array())
+    private function get()
     {
-        $this->setup();
-        //$this->setOption(CURLOPT_RETURNTRANSFER, true);
-        $this->requestOptions[CURLOPT_RETURNTRANSFER] = 1;
-
-        return $this->request($url, $data);
+        return $this->request();
     }
 
     /**
@@ -227,14 +460,12 @@ class Client
      * 
      * @return void
      */
-    private function post($url, $data = array())
+    private function post()
     {
-        $this->setup();
-        //$this->setOption(CURLOPT_POST, 1);
-        $this->requestOptions[CURLOPT_POST] = 1;
-        $this->setPostFields($data);
+        $this->setPostFields();
+        $this->setOption(CURLOPT_POST, 1);
 
-        return $this->request($url);
+        return $this->request();
     }
 
     /**
@@ -242,12 +473,14 @@ class Client
      * 
      * @return void
      */
-    private function setPostFields($data)
+    private function setPostFields()
     {
-        if (is_array($data)) {
-            //$this->setOption(CURLOPT_POSTFIELDS, $data);
-            $this->requestOptions[CURLOPT_POSTFIELDS] = $data;
+        if (!empty($this->fields))
+        {
+            $this->setOption(CURLOPT_POSTFIELDS, http_build_query($this->fields, '', '&'));
         }
+
+        return $this;
     }
 
     /**
@@ -255,13 +488,12 @@ class Client
      * 
      * @return void
      */
-    private function delete($url, $data = array())
+    private function delete()
     {
-        $this->setup();
-        $this->requestOptions[CURLOPT_CUSTOMREQUEST] = 'DELETE';
-        $this->setPostFields($data);
-        
-        return $this->request($url);
+        $this->setPostFields();
+        $this->setOption(CURLOPT_CUSTOMREQUEST, 'DELETE');
+
+        return $this->request();
     }
 
     /**
@@ -269,32 +501,38 @@ class Client
      * 
      * @return void
      */
-    private function put($url, $data = array())
+    private function put()
     {
-        $this->setup();
+        $this->setPostFields();
+        $this->setOption(CURLOPT_CUSTOMREQUEST, 'PUT');
 
-        //$this->setOption(CURLOPT_CUSTOMREQUEST, 'PUT');
-        $this->requestOptions[CURLOPT_CUSTOMREQUEST] = 'PUT';
-        $this->setPostFields($data);
-        //$this->setUrl($url);
-        
-        return $this->request($url);
+        return $this->request();
     }
 
     /**
-     * Setup request
+     * Reset request settings
      * 
      * @return void
      */
-    private function setup()
+    private function reset()
     {
-        $this->response       = '';
-        $this->info           = array();
-        $this->headers        = array();
-        $this->options        = $this->defaultOptions;
-        $this->requestOptions = array();
-        //$this->errorCode    = 0;
-        //$this->errorString  = '';
+        $this->options = array(
+            CURLOPT_CONNECTTIMEOUT => 5,
+            CURLOPT_TIMEOUT        => 10,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_FOLLOWLOCATION => 1,
+            CURLOPT_FAILONERROR    => 0,
+            CURLOPT_ENCODING       => 'utf-8',
+            CURLOPT_USERAGENT      => 'SlimSurveys-API-Client',
+        );
+
+        $this->response = '';
+        $this->info     = array();
+        $this->headers  = array();
+        $this->queries  = array();
+        $this->fields   = array();
+        
+        return $this;
     }
 
     /**
@@ -302,52 +540,82 @@ class Client
      * 
      * @return json
      */
-    public function request($url, $params = array())
+    private function request()
     {
-        // todo: file upload
-        // todo: may need to build oauth header
-
-        $this->setOptions($this->requestOptions);
-
-        $this->buildUrl($url, $params);
-        $this->setHeader('X-API-KEY', $this->key);
-        $this->setOption(CURLOPT_HTTPHEADER, $this->buildHeader());
+        $this->buildUrl();
+        $this->buildHeader();
 
         $curl = curl_init();
 
-        curl_setopt_array($curl, $this->getOptions());
+        curl_setopt_array($curl, $this->options);
 
         $this->response = curl_exec($curl);
-        $this->info     = curl_getinfo($curl);
 
-        if (false === $this->response) {
-            $errorCode    = curl_errno($curl);
-            $errorMessage = curl_error($curl);
-            
-            //var_dump($errorCode);
-            //var_dump($errorMessage);
-
-            // todo: populate exception, close session, throw exception
-            // throw new SlimSurveysApiException($errorMessage, $errorCode);
+        if (false !== $this->response)
+        {
+            $data = json_decode($this->response);
         }
+
+        $this->data = $data;
+        $this->info = curl_getinfo($curl);
+
+        $this->info['error_code']    = curl_errno($curl);
+        $this->info['error_message'] = curl_error($curl);
 
         curl_close($curl);
 
+        return $this;
+    }
+
+    /**
+     * Make api request
+     * 
+     * @return json
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * Get raw response
+     * 
+     * @return json
+     */
+    public function getResponse()
+    {
         return $this->response;
     }
 
-}
+    /**
+     * Get request info
+     * 
+     * @return array|string
+     */
+    public function getInfo($key = null)
+    {
+        if (isset($key))
+        {
+            return (array_key_exists($key, $this->info)) ? $this->info[$key] : null;
+        }
 
-/*
- * Slim surveys api exception.
- */
-class ApiException extends \Exception {
-    // todo: integrate exception handling
+        return $this->info;
+    }
+
+    /**
+     * Get http response code
+     * 
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->getInfo('http_code');
+    }
 }
 
 /*
  * Slim surveys exception.
  */
 if (!function_exists('curl_init')) {
-    throw new Exception('SlimSurveys needs the CURL PHP extension.');
+    throw new Exception('SlimSurveys API client requires the CURL PHP extension.');
 }
